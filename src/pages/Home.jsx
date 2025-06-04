7e1477dbd7e9805743763076b46119d034ca0bcb
Rayan Khaleel Racki Abu Ali <rayan@weg.net>
2025-06-04 17:49:21 -0300

    Background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
+import { Star } from "lucide-react"
 import { ThemeToggle } from "../components/ThemeToggle"
+import { StarBackground } from "../components/StarBackground"
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-hidden">
         <ThemeToggle />
+
+        <StarBackground />
         
     </div>
 }
